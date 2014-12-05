5d8f697bc88c28ed806afd80690bfc44e0f0cd5f
Vitali Nartov <divapps@gmail.com>
2014-12-05 12:34:48 +0200

    Scaling improvements

--- app/src/main/java/com/ap/androidltest/widget/BaseGalleryLayoutManager.java
+++ app/src/main/java/com/ap/androidltest/widget/BaseGalleryLayoutManager.java
@@ -164,7 +164,7 @@ public abstract class BaseGalleryLayoutManager extends RecyclerView.LayoutManage
             int right = getDecoratedRight(child);
             float center = (float) (left + (right - left) / 2);
             float halfScreen = (float) getWidth() / 2;
-            float distanceFromCenter = Math.abs(halfScreen - center) / 2; // divided by 2 to start resizing earlier
+            float distanceFromCenter = Math.abs(halfScreen - center) / 4; // divided by 2 to start resizing earlier
             float scale = 1 - distanceFromCenter / halfScreen;
             if (scale < 0) scale = 0;
             float alpha = scale;
